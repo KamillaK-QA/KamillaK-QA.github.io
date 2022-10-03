1f04489e83ed6dbaa8fdd1f43cbb4a4fe0701422
KamillaK-QA <114947313+KamillaK-QA@users.noreply.github.com>
2022-10-03 21:49:14 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert(' 4500 часов или 187 дней ')
+})
